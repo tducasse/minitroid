e1e2159f25b449d249997373d542c15c1b41e293
Thibaud Ducasse <thibaud.ducasse@gmail.com>
2021-08-10 22:07:20 +1000

    Add Mother interaction

--- src/entities/mother.lua
+++ src/entities/mother.lua
@@ -9,6 +9,27 @@ function Mother:update(dt)
   self.sprite:update(dt)
 end
 
+function Mother:hit()
+  self.hp = self.hp - 1
+  if self.hp == 6 then
+    self.sprite:setTag("Broken")
+  elseif self.hp > 0 then
+    self.sprite:setTag("Hurt")
+  else
+    self:destroy()
+  end
+end
+
+function Mother:destroy()
+  Signal.emit(SIGNALS.DESTROY_ITEM, self, self.collection)
+end
+
+function Mother:onLoop()
+  if self.sprite.tagName == "Hurt" then
+    self.sprite:setTag("Broken")
+  end
+end
+
 function Mother:new(m, collection)
   self.type = "mother"
   self.collection = collection
@@ -19,11 +40,14 @@ function Mother:new(m, collection)
   self.w = m.w
   self.h = m.h
 
+  self.hp = 7
+
   -- DRAWING
   self.sprite = peachy.new(
                     "assets/mother-brain.json",
                     love.graphics.newImage("assets/mother-brain.png"), "Alive")
   self.sprite:play()
+  self.sprite:onLoop(self.onLoop, self)
 end
 
 return Mother
